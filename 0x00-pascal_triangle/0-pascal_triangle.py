4fd1306b76f187cf2266a30e2c3c0ab32867ff6f
mostafa-abokhadra <mvxizk@gmail.com>
2024-07-28 01:13:46 +0300

    my pascal test :)

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" ... """
-
-
 def pascal_triangle(n):
-    """ ... """
     if n <= 0:
         return []
-
-    triangle = [[1]]
-
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
-        triangle.append(row)
-
-    return triangle
+    a_list = []
+    pascal = [[1], [1, 1]]
+    idx = 0
+    for i in range(3, n + 1):
+        a_list = [None] * i
+        a_list[0] = 1
+        a_list[-1] = 1
+        idx = 1
+        for j in range(0, len(pascal[-1])):
+            if j == len(pascal[-1]) - 1:
+                break;
+            a_list[idx] = pascal[-1][j] + pascal[-1][j + 1]
+            idx += 1
+        pascal.append(list(a_list))
+    return pascal
