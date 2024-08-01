d15c86409e0a663eaf79701a409aa721a749ab5d
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-01 21:39:26 +0300

    optimized code allow for access any box even if you don't have it's key

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,19 +9,24 @@
     The first box boxes[0] is unlocked.
     task: determines if all the boxes can be opened.
 """
-
-
-def solve(boxes, oneBox, still_locked):
-    if len(still_locked) == 0:
-        return True
-    for key in oneBox:
-        if not(key in still_locked):
-            continue
-        still_locked.remove(key)
-        solve(boxes, boxes[key], still_locked)
-        if len(still_locked) == 0:
-            return True
-    return False
+# def solve(boxes, oneBox, still_locked):
+#     if len(still_locked) == 0:
+#         return True
+#     for key in oneBox:
+#         if not(key in still_locked):
+#             continue
+#         still_locked.remove(key)
+#         solve(boxes, boxes[key], still_locked)
+#         if len(still_locked) == 0:
+#             return True
+#     return False
+# def canUnlockAll(boxes):
+# if (type(boxes)) is not list:
+#     return False
+# elif (len(boxes)) == 0:
+#     return False
+# all_locked = [i for i in range(1, len(boxes))]
+# return solve(boxes, boxes[0], all_locked)
 
 
 def canUnlockAll(boxes):
@@ -39,5 +44,13 @@ def canUnlockAll(boxes):
         return False
     elif (len(boxes)) == 0:
         return False
-    all_locked = [i for i in range(1, len(boxes))]
-    return solve(boxes, boxes[0], all_locked)
+
+    for k in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for idx in range(len(boxes)):
+            boxes_checked = k in boxes[idx] and k != idx
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True
