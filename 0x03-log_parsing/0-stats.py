7ce06e90a5ab499f5becb47d8c192551eecb4eb1
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-18 23:03:08 +0300

    ok

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,60 +1,49 @@
 #!/usr/bin/python3
-""" Log parsing """
+"""Log parsing"""
+
+
+import sys
 import re
 
 
-def check_input(line):
-    """ doc """
-    regex = r'^[\S]+\s*-\s*\[\d{4}-\d\d-\d\d\s*\d\d:\d\d:\d\d\.\d{6}\]'
-    regex2 = r'\s*"GET \/projects\/260 HTTP\/1\.1"\s*(\S+)\s*(\d+)$'
-    match = re.match(regex + regex2, line)
-
-    if match:
-        status_code = match.group(1)
-        file_size = int(match.group(2))
-
-        return [status_code, file_size]
-    return []
-
-
-def print_data(file_size=0, data={}):
-    """ doc """
-    print(f'File size: {file_size}', flush=True)
-    for key in sorted(data.keys()):
-        if data[key] == 0:
-            continue
-        print(f'{key}: {data[key]}', flush=True)
-
-
-def show_status():
-    """ doc """
-    status_code = {
-        '200': 0, '301': 0,
-        '400': 0, '401': 0,
-        '403': 0, '404': 0,
-        '405': 0, '500': 0,
+def log_parsing():
+    """Log parsing"""
+    reg = re.compile(r"""(\d+\.\d+\.\d+\.\d+) - \[([^\]]+)\] \"GET /projects/260 HTTP/1.1\" (\d+) (\d+)""")  # noqa
+    total_size = 0
+    status_codes = {
+        200: 0,
+        301: 0,
+        400: 0,
+        401: 0,
+        403: 0,
+        404: 0,
+        405: 0,
+        500: 0
     }
-    file_size = 0
-    counter = 0
-    try:
-        while True:
-            line = input()
-            data = check_input(line)
-            if len(data) == 0:
-                continue
-            if data[0] in status_code.keys():
-                status_code[data[0]] += 1
-            file_size += data[1]
-            counter += 1
-            if counter == 10:
-                print_data(file_size, status_code)
-                counter = 0
+    line_count = 0
 
-    except (KeyboardInterrupt, EOFError):
-        print_data(file_size, status_code)
-        counter = 0
+    for line in sys.stdin:
+        line_count += 1
+        match = reg.match(line)
+
+        if match:
+            total_size += int(match.group(4))
+            status = int(match.group(3))
+
+            if status in status_codes:
+                status_codes[status] += 1
+
+        if line_count % 10 == 0:
+            print("File size: {}".format(total_size))
+            for key, value in sorted(status_codes.items()):
+                if value:
+                    print("{}: {}".format(key, value))
+
+    print("File size: {}".format(total_size))
+    for key, value in sorted(status_codes.items()):
+        if value:
+            print("{}: {}".format(key, value))
 
 
 if __name__ == "__main__":
-    """ main function """
-    show_status()
+    log_parsing()
