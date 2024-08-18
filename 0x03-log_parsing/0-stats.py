83f1b249d7a07b0f5103bdff1c927c3ce349a25c
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-18 23:00:37 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,83 +1,60 @@
 #!/usr/bin/python3
-""" log parsing
-"""
+""" Log parsing """
+import re
 
 
-# import functools
-# import sys
-# def add(a, b):
-#     """add 2 numbers
-#     """
-#     return a + b
-# def metrics():
-#     """
-#     <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
-#     <status code> <file size>
-#     127.0.0.1 - [1/1/2024] "GET /projects/260 HTTP/1.1" 200 4000
-#     """
-#     counter = 0
-#     stausCodes = [200, 301, 400, 401, 403, 404, 405, 500]
-#     fileSizes = [0, 0]
-#     statusNumbers = {}
-#     try:
-#         for line in sys.stdin:
-#             splitted = line.split()
-#             if not len(splitted) == 9:
-#                 continue
-#             else:
-#                 fileSizes.append(int(splitted[-1]))
-#                 try:
-#                     status = int(splitted[-2])
-#                     if status not in stausCodes:
-#                         continue
-#                     if status in statusNumbers.keys():
-#                         statusNumbers[status] += 1
-#                     else:
-#                         statusNumbers[status] = 1
-#                 except Exception as err:
-#                     continue
-#             counter += 1
-#             if counter == 10:
-#                 break
-#     except KeyboardInterrupt:
-#         pass
-#     print("File size: {}".format(functools.reduce(add, fileSizes)))
-#     for key, value in sorted(statusNumbers.items()):
-#         print("{}: {}".format(key, value))
-# if __name__ == '__main__':
-#     metrics()
-import sys
+def check_input(line):
+    """ doc """
+    regex = r'^[\S]+\s*-\s*\[\d{4}-\d\d-\d\d\s*\d\d:\d\d:\d\d\.\d{6}\]'
+    regex2 = r'\s*"GET \/projects\/260 HTTP\/1\.1"\s*(\S+)\s*(\d+)$'
+    match = re.match(regex + regex2, line)
+
+    if match:
+        status_code = match.group(1)
+        file_size = int(match.group(2))
+
+        return [status_code, file_size]
+    return []
 
 
-if __name__ == '__main__':
+def print_data(file_size=0, data={}):
+    """ doc """
+    print(f'File size: {file_size}', flush=True)
+    for key in sorted(data.keys()):
+        if data[key] == 0:
+            continue
+        print(f'{key}: {data[key]}', flush=True)
 
-    filesize, count = 0, 0
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    stats = {k: 0 for k in codes}
 
-    def print_stats(stats: dict, file_size: int) -> None:
-        print("File size: {:d}".format(filesize))
-        for k, v in sorted(stats.items()):
-            if v:
-                print("{}: {}".format(k, v))
-
+def show_status():
+    """ doc """
+    status_code = {
+        '200': 0, '301': 0,
+        '400': 0, '401': 0,
+        '403': 0, '404': 0,
+        '405': 0, '500': 0,
+    }
+    file_size = 0
+    counter = 0
     try:
-        for line in sys.stdin:
-            count += 1
-            data = line.split()
-            try:
-                status_code = data[-2]
-                if status_code in stats:
-                    stats[status_code] += 1
-            except BaseException:
-                pass
-            try:
-                filesize += int(data[-1])
-            except BaseException:
-                pass
-            if count % 10 == 0:
-                print_stats(stats, filesize)
-        print_stats(stats, filesize)
-    except KeyboardInterrupt:
-        print_stats(stats, filesize)
-        raise
+        while True:
+            line = input()
+            data = check_input(line)
+            if len(data) == 0:
+                continue
+            if data[0] in status_code.keys():
+                status_code[data[0]] += 1
+            file_size += data[1]
+            counter += 1
+            if counter == 10:
+                print_data(file_size, status_code)
+                counter = 0
+
+    except (KeyboardInterrupt, EOFError):
+        print_data(file_size, status_code)
+        counter = 0
+
+
+if __name__ == "__main__":
+    """ main function """
+    show_status()
